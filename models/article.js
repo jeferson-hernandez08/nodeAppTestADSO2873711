01281b437fda30ab46a78eacc856d7ed3b2b2188
Jeferson <jefer.hernandez1@gmail.com>
2025-05-28 16:23:35 -0500

    Modificacion de article.js

--- models/article.js
+++ models/article.js
@@ -4,29 +4,35 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Article extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
-      Article.belongsTo(models.User);   // Un articulo pertenece a un usuario
-      models.User.hasMany(Article);    // Un usuario tiene muchos articulos
-      // Un articulo puede pertenecer a muchas categorias y una categoria puede tener muchos articulos
+      Article.belongsTo(models.User, {
+        foreignKey: 'userId', // Asegúrate de especificar la foreignKey
+        as: 'User'
+      });
+      
+      models.User.hasMany(Article, {
+        foreignKey: 'userId'
+      });
+      
       Article.belongsToMany(models.Category, {
-        through: 'articleCategories',  // Tabla intermedia
-        as: 'categories',              // Nombre del alias para la relación
+        through: 'articleCategories',
+        as: 'categories',
+        foreignKey: 'articleId' // Especifica foreignKey explícitamente
       });
     }
   }
   Article.init({
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
-    userId: DataTypes.INTEGER
+    userId: { // Define explícitamente con opciones
+      type: DataTypes.INTEGER,
+      field: 'UserId' // Fuerza el nombre exacto de la columna en la BD
+    }
   }, {
     sequelize,
     modelName: 'Article',
+    tableName: 'Articles', // Asegúrate que coincide con el nombre real de la tabla
+    underscored: false // Desactiva snake_case conversion
   });
   return Article;
 };
