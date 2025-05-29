abb94597c16660a98ce16b84b4e2fe55df6f07db
Jeferson <jefer.hernandez1@gmail.com>
2025-05-28 19:32:38 -0500

    M-13

--- models/category.js
+++ models/category.js
@@ -14,8 +14,7 @@ module.exports = (sequelize, DataTypes) => {
       Category.belongsToMany(models.Article, {
         through: 'articleCategories',  // Tabla intermedia, tal cual como en la base de datos
         as: 'articles',                // Nombre del alias para la relación
-        foreignKey: 'categoryId', // 👈 minúsculas
-        otherKey: 'articleId'     // 👈 minúsculas👨🏼
+        
       });
     }
   }
