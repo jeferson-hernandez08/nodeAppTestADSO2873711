100043a99206de1f80abff309df80155c11194ae
Jeferson <jefer.hernandez1@gmail.com>
2025-05-28 19:50:50 -0500

    M-13

--- models/category.js
+++ models/category.js
@@ -14,7 +14,8 @@ module.exports = (sequelize, DataTypes) => {
       Category.belongsToMany(models.Article, {
         through: 'articleCategories',  // Tabla intermedia, tal cual como en la base de datos
         as: 'articles',                // Nombre del alias para la relación
-        
+        foreignKey: 'categoryId', // 👈 Mandamos a sequielize las variables de nuestra tabla articleCategories
+        otherKey: 'articleId'     // 👈 
       });
     }
   }
